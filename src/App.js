dea7db8e3276adc7642273a5cfcd4ae7d4328cac
Jose Emanuel Delgado <emadevjs@gmail.com>
2021-12-07 11:36:26 -0300

    fix bugs 1.1

--- src/App.js
+++ src/App.js
@@ -1,61 +1,68 @@
-import React, {useState, useEffect} from 'react'
-import {Recipe} from './components/Recipe';
+import React, { useState, useEffect } from "react";
+import { Recipe } from "./components/Recipe";
 import "./App.css";
 
 export function App() {
-
   const APP_ID = "d00025be";
   const APP_KEY = "1c102e662dd5e7860c98abded7349dac";
   const [recipes, setRecipes] = useState([]);
   const [search, setSearch] = useState("");
   const [query, setQuery] = useState("meet");
 
-  useEffect( ()=>{
+  useEffect(() => {
     getRecipes();
-  },[query]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [query]);
 
-  const getRecipes = async () =>{
+  const getRecipes = async () => {
     const response = await fetch(
       `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`
-      );
+    );
     const data = await response.json();
     setRecipes(data.hits);
     console.log(data.hits);
   };
 
-  const handleSearch= e =>{
+  const handleSearch = (e) => {
     setSearch(e.target.value);
   };
 
-  const getSearch= (e) =>{
+  const getSearch = (e) => {
     e.preventDefault();
-    setQuery(search); 
+    setQuery(search);
     setSearch("");
   };
-  
-   return (
+
+  return (
     <div className="App">
       <form onSubmit={getSearch} className="search-form">
-        <input className="search-bar"
-         type="text"
-         value={search}
-         onChange={handleSearch}
-         placeholder="Insert a recipe to search..."></input>
-        <button className="search-button" type="submit">Search</button>
+        <input
+          className="search-bar"
+          type="text"
+          value={search}
+          onChange={handleSearch}
+          placeholder="Insert a recipe to search..."
+        ></input>
+        <button className="search-button" type="submit">
+          Search
+        </button>
       </form>
       <div className="recipes">
-      {recipes ? recipes.map(recipe =>(
-        <Recipe
-        key={recipe.recipe.uri}
-        title={recipe.recipe.label}
-        calories={Math.round(recipe.recipe.calories)}
-        imgUrl={recipe.recipe.image}
-        ingredients={recipe.recipe.ingredients}
-        url={recipe.recipe.url}/>
-      )): <h1>Wait a minute and try again please.</h1>}
+        {recipes ? (
+          recipes.map((recipe) => (
+            <Recipe
+              key={recipe.recipe.uri}
+              title={recipe.recipe.label}
+              calories={Math.round(recipe.recipe.calories)}
+              imgUrl={recipe.recipe.image}
+              ingredients={recipe.recipe.ingredients}
+              url={recipe.recipe.url}
+            />
+          ))
+        ) : (
+          <h1>Wait a minute and try again please.</h1>
+        )}
       </div>
     </div>
-  )
+  );
 }
-
-
