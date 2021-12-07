d670c9da7fc713062028195723ad66e94b34cfce
Jose Emanuel Delgado <emadevjs@gmail.com>
2021-12-07 11:32:18 -0300

    fix bugs

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,20 +1,22 @@
-import React from 'react';
-import style from '.recipe.module.css';
+import React from "react";
+import "./recipe.css";
 
-export function Recipe({title, calories, imgUrl, ingredients,url}) {
-    return (
-       
-            <div className={style.recipe}>
-                <img className={style.image} src={imgUrl} alt=""></img>
-                <h1 className={style.title}>{title}</h1>
-                <ol className={style.ol}>
-                    {ingredients.map((ingredient, idx)=>(
-                        <li className={style.li} key={idx} >{ingredient.text}</li>
-                    ))}
-                </ol>
-                <p className={style.calories}>Calories = {calories}</p>
-                <a className={style.url} href={url}>👨‍🍳How to prepare</a>
-            </div>
-       
-    )
+export function Recipe({ title, calories, imgUrl, ingredients, url }) {
+  return (
+    <div className="recipe">
+      <img className="image" src={imgUrl} alt=""></img>
+      <h1 className="title">{title}</h1>
+      <ol className="ol">
+        {ingredients.map((ingredient, idx) => (
+          <li className="li" key={idx}>
+            {ingredient.text}
+          </li>
+        ))}
+      </ol>
+      <p className="calories">Calories = {calories}</p>
+      <a className="url" href={url}>
+        👨‍🍳How to prepare
+      </a>
+    </div>
+  );
 }
